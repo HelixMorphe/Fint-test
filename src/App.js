f3e7399b73561547757319a5a6eb2c4c6b9ac69c
Skillcet <skillsntools.web@gmail.com>
2021-11-13 14:52:28 +0530

    lb updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-
+import ReactGa from "react-ga";
 function App() {
+  useEffect(() => {
+    ReactGa.initialize("G-S22Z9M1X0Z");
+    //to report page views
+
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div>
       <Home />
